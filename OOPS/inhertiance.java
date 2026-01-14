abee9ee40ac788b64494780cae1a6a60086ab701
surajpillai11 <pillai.suraj004@gmail.com>
2026-01-14 19:19:19 +0530

    ---

---
+++ OOPS/inhertiance.java
@@ -0,0 +1,19 @@
+class Calc 
+{
+    public int add(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+    public int sub(int n1, int n2)
+    {
+        return n1 - n2;
+    }
+}
+
+public class inhertiance 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+}
