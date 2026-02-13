81418b1f185cbaa54bd12f9effafaf0864d7d106
surajpillai11 <pillai.suraj004@gmail.com>
2026-02-13 22:45:08 +0530

    Polymorphism

---
+++ OOPS/poly.java
@@ -0,0 +1,38 @@
+class A {
+    public void show()
+    {
+        System.out.println("in A show");
+    }
+    
+}
+
+class B extends A {
+    public void show()
+    {
+        System.out.println("in B show");
+    }
+    
+}
+
+class C extends A {
+    public void show()
+    {
+        System.out.println("in C show");
+    }
+    
+}
+
+
+
+public class poly {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+
+        obj = new B();  //This is call Run time Polymorphism
+        obj.show();
+
+        obj = new C();
+        obj.show();
+    }
+}
